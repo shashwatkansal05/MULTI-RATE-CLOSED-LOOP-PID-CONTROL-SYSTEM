0d2a8037c4b0ea2e907e8bbe37c4e4b5a4c09595
Shashwat Kansal <shashwatkansal05@gmail.com>
2026-02-26 09:41:45 +0530

    Create FreeRtos.h

---
+++ FreeRtos.h
@@ -0,0 +1,14 @@
+#ifndef FREERTOS_H
+#define FREERTOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define pdMS_TO_TICKS(x) (x)
+#define pdTRUE 1
+#define pdFALSE 0
+
+typedef unsigned int TickType_t;
+
+#endif
